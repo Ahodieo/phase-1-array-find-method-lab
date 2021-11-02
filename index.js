ba8ca6da1bb611356ed7d433f07f7d75568637a4
Jahson Diego <jdiego847@gmail.com>
2021-11-02 14:16:51 -0400

    commit tree 1d5c469b791c557426eb5b2946cd32bb3ddc400a

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+function superbowlWin(record){
+    function win(something){
+        if (something.result === "W")
+        return something.result;
+    else
+        return undefined;}
+    let answer = record.find(win);
+    if (answer === undefined)
+        return answer
+    else
+    return answer.year;
+    }
